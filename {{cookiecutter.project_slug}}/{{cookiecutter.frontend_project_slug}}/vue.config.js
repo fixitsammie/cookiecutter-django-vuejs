d16954a9792bf67ea0961c6c532df18782c9de2d
Samuel Adewole <samadewole381@gmail.com>
2022-06-24 00:10:17 +0100

    test placeholder replacement in dockerfile

--- {{cookiecutter.project_slug}}/{{cookiecutter.frontend_project_slug}}/vue.config.js
+++ {{cookiecutter.project_slug}}/{{cookiecutter.frontend_project_slug}}/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://localhost:8000'
+    proxy: 'http://localhost:8000',
+    host: '0.0.0.0',
+    port: 8080, 
   }
 })
